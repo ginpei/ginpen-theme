3c4c88bcad2a3077ba7d628ed88a1b8ad6c836ae
Takanashi Ginpei <ginpei@gmail.com>
2012-04-15 00:30:29 +0900

    google analytics.

--- footer.php
+++ footer.php
@@ -12,3 +12,17 @@
 </address>
 <div id="site-generator"><a href="<?php echo esc_url( 'http://wordpress.org/' ); ?>" rel="generator">Proudly powered by WordPress</a></div>
 <script src="<?php echo get_template_directory_uri(); ?>/js/global.js" type="text/javascript"></script>
+<?php if (!is_user_logged_in()) : ?>
+<!-- Google Analytics -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-21122526-1']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- /Google Analytics -->
+<?php endif; ?>
