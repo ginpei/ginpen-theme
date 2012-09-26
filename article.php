1110e11b99f426f66df5af010a2ef2cbb5bfbee6
Takanashi Ginpei <ginpei@gmail.com>
2012-09-27 02:37:49 +0900

    show addthis buttons under article

--- article.php
+++ article.php
@@ -34,6 +34,19 @@
   </div>
   <footer>
     <?php if ( is_single() ) : ?>
+			<div class="addthis">
+<!-- AddThis Button BEGIN -->
+<div class="addthis_toolbox addthis_default_style "
+  addthis:url="<?php the_permalink(); ?>"
+  addthis:title="<?php the_title_attribute(); ?>">
+<a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+<a class="addthis_button_tweet"></a>
+<a class="addthis_button_google_plusone" g:plusone:size="medium"></a>
+<a class="addthis_counter addthis_pill_style"></a>
+</div>
+<?php wp_enqueue_script( 'addthis', 'http://s7.addthis.com/js/250/addthis_widget.js#pubid=ginpei' ); ?>
+<!-- AddThis Button END -->
+			</div>
       <div class="comments-wrapper well">
         <?php comments_template( '', true ); ?>
         <script type="text/javascript">
