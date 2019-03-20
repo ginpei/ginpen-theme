b8160fece21a8eb76d8bec58c55d2caeb7c24838
ginpei <ginpei@gmail.com>
2019-03-20 14:55:52 +0900

    follow terms as long as I understood

--- footer.php
+++ footer.php
@@ -11,6 +11,6 @@
   <a href="https://twitter.com/ginpei_jp">@ginpei_jp</a>
 </address>
 <div id="site-generator"><a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>" rel="generator">Proudly powered by WordPress</a></div>
-<div><a href="https://marketingplatform.google.com/about/analytics/">Access tracked by Google Analytics</a></div>
+<div><a href="https://www.google.com/intl/ja/policies/privacy/partners/">Access tracked by Google Analytics using cookie</a></div>
 <script src="https://ginpen.com/lab/jQuery.gpHatebuCounter/jquery.gphatebucounter-1.1.min.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/global.js" type="text/javascript"></script>
