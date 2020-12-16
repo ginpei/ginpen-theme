90125f603c4da2ace4ec718869ed66e2d79c5494
ginpei <ginpei@gmail.com>
2020-12-16 11:57:22 -0800

    update to stop refusing zoom

--- meta.php
+++ meta.php
@@ -1,6 +1,6 @@
 <?php do_action( 'get_header' ); ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+<meta name="viewport" content="width=device-width">
 <title><?php _meta_echo_title(); ?></title>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" />
 <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>?0401" />
