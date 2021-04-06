3f1072033a31bb0a211660ad3f36043f6673272d
ginpei <ginpei@ginpei.dev>
2021-04-06 10:27:19 -0700

    remove all styles and scripts

--- meta.php
+++ meta.php
@@ -1,23 +1,13 @@
+
 <?php do_action( 'get_header' ); ?>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width">
 <title><?php _meta_echo_title(); ?></title>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/vendor/google-fonts/Slackey.css" />
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" />
-<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>?0401" />
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-<?php if ( is_single() ) : ?>
-  <link rel="alternate" media="handheld" href="<?php the_permalink(); ?>">
-<?php endif; ?>
-<!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-<![endif]-->
-<?php wp_deregister_script('jquery'); ?>
-<?php wp_enqueue_script('jquery', get_template_directory_uri() . '/vendor/jquery/jquery-3.5.1.slim.min.js'); ?>
-<?php if ( is_singular() && get_option( 'thread_comments' ) ) : ?>
-  <?php wp_enqueue_script( 'comment-reply' ); ?>
-<?php endif; ?>
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+
 <?php wp_head(); ?>
+
 <?php // ------------------------------------------------------------------
 /**
  * Write site title.
