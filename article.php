cbd9f6d3224416981f3f23cb7d267fc1ffb8f3a0
ginpei <ginpei@ginpei.dev>
2021-04-07 16:33:31 -0700

    update article design

--- article.php
+++ article.php
@@ -1,48 +1,43 @@
-<article>
+<article class="article">
   <header>
     <div class="u-container">
-      <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-      <div class="article-status well">
-        <div class="post-datetime">投稿日時: <a href="<?php the_permalink() ?>"><time datetime="<?php the_time('c'); ?>"><?php the_time('Y/m/d H:i'); ?></time></a></div>
+      <h1 class="article-title">
+        <a class="article-titleLink" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+      </h1>
+      <div class="article-status">
+        <span class="article-statusItem">
+          投稿日時: <a href="<?php the_permalink() ?>"><time datetime="<?php the_time('c'); ?>"><?php the_time('Y/m/d H:i'); ?></time></a>
+        </span>
         <?php if ( get_post_type() == 'post') : ?>
-          <div class="post-categories">
+          <span class="article-statusItem">
             カテゴリー:
             <?php the_category(', '); ?>
-          </div>
-          <div class="post-tags">
+          </span>
+          <span class="article-statusItem">
             <?php the_tags(); ?>
-          </div>
+          </span>
         <?php endif; ?>
-        <?php edit_post_link(); ?>
+        <?php if (is_user_logged_in()) : ?>
+          <span class="article-statusItem">
+            <?php edit_post_link(); ?>
+          </span>
+        <?php endif; ?>
       </div>
-      <div class="addthis">
-        <?php get_template_part( 'components/addthis' ); ?>
-      </div>
+      <?php get_template_part( 'components/addthis' ); ?>
     </div>
   </header>
-  <?php if (!is_ssl()) { ?>
+  <div class="article-body">
     <div class="u-container">
-      <p class="notice-sslLink">
-        ※この記事にはHTTPS版のURLがあります。よろしければそちらをご利用ください。
-        <br />
-        <a href="<?php the_permalink(); ?>"><?php the_permalink(); ?></a>
-      </p>
-    </div>
-  <?php } ?>
-  <div class="u-container">
-    <div class="article-body">
       <?php the_content(); ?>
       <?php if ( is_single() ) wp_link_pages('before=<nav class="pagination">ページ: &after=</nav>&pagelink=<span class="page">%</span>'); ?>
     </div>
   </div>
   <footer>
     <?php if ( is_single() ) : ?>
-      <div class="addthis">
-        <?php get_template_part( 'components/addthis' ); ?>
-      </div>
+      <?php get_template_part( 'components/addthis' ); ?>
       <nav class="post-neighbers nav">
-        <?php next_post_link('<span class="next well">%link</span>', '&larr; %title'); ?>
-        <?php previous_post_link('<span class="prev well">%link</span>', '%title &rarr;'); ?>
+        <?php // next_post_link('<span class="next well">%link</span>', '&larr; %title'); ?>
+        <?php // previous_post_link('<span class="prev well">%link</span>', '%title &rarr;'); ?>
       </nav>
     <?php endif; ?>
   </footer>
