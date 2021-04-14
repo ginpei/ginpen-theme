6039c19ddc622d00d044a5b2fd2080003cfd5f67
ginpei <ginpei@ginpei.dev>
2021-04-14 01:51:47 -0700

    refine JS file handling

--- footer.php
+++ footer.php
@@ -12,6 +12,5 @@
     </address>
     <p id="site-generator"><a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>" rel="generator">Proudly powered by WordPress</a></p>
     <p><a href="https://www.google.com/intl/ja/policies/privacy/partners/">Access tracked by Google Analytics using cookie</a></p>
-    <script src="<?php echo get_template_directory_uri(); ?>/js/global.js" type="module"></script>
   </div>
 </div>
