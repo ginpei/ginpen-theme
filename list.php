5d225cd7693a214f2d8812051c13d35d42239e86
ginpei <ginpei@ginpei.dev>
2021-04-14 23:07:16 -0700

    better naming

--- list.php
+++ list.php
@@ -4,10 +4,10 @@
       <?php while ( have_posts() ) : the_post(); ?>
         <li class="list-item">
           <a class="list-link" href="<?php the_permalink() ?>">
-            <time class="list-linkTime" datetime="<?php the_time('c'); ?>">
+            <time class="list-postTime" datetime="<?php the_time('c'); ?>">
               <?php the_time('Y/m/d H:i'); ?>
             </time>
-            <span class="list-linkTitle"><?php the_title() ?></span>
+            <span class="list-postTitle"><?php the_title() ?></span>
           </a>
         </li>
       <?php endwhile; ?>
