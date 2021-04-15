492d06a74571c31497ebc8a1d829bd0e494b4b64
ginpei <ginpei@ginpei.dev>
2021-04-14 23:05:06 -0700

    update markup

--- list.php
+++ list.php
@@ -4,12 +4,9 @@
       <?php while ( have_posts() ) : the_post(); ?>
         <li class="list-item">
           <a class="list-link" href="<?php the_permalink() ?>">
-            <small class="list-asideLinkText">
-              <time datetime="<?php the_time('c'); ?>">
-                <?php the_time('Y/m/d H:i'); ?>
-              </time>
-            </small>
-            <br/>
+            <time class="list-linkTime" datetime="<?php the_time('c'); ?>">
+              <?php the_time('Y/m/d H:i'); ?>
+            </time>
             <span class="list-linkTitle"><?php the_title() ?></span>
           </a>
         </li>
