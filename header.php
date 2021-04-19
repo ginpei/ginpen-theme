bca244971a7e27abf5402b41dfdedd318ada3cd1
ginpei <ginpei@ginpei.dev>
2021-04-19 11:27:10 -0700

    fix unpredictable clickable area

--- header.php
+++ header.php
@@ -10,32 +10,42 @@
       </a>
     </div>
     <div class="header-infoList">
-      <a class="header-infoItem" href="/about/">
-        <span class="header-infoIcon">
-          <img src="<?= get_template_directory_uri() ?>/img/icon-32.png" width="16" height="16" alt="" />
-        </span>
-        高梨ギンペイ
-      </a>
-      <a class="header-infoItem" href="<?php bloginfo('rss2_url'); ?>">
-        <span class="header-infoIcon">📻</span>
-        RSS
-      </a>
-      <a class="header-infoItem header-infoLastItemInLine" href="https://www.google.co.jp/search?q=site%3Aginpen.com">
-        <span class="header-infoIcon">🔍</span>
-        検索
-      </a>
-      <a class="header-infoItem" href="https://twitter.com/ginpei_jp">
-        <span class="header-infoIcon">
-          <img src="<?php echo get_template_directory_uri(); ?>/img/Twitter_Logo_Blue.svg" width="16" height="16" alt="" />
-        </span>
-        @ginpei_jp
-      </a>
-      <a class="header-infoItem" href="https://github.com/ginpei/">
-        <span class="header-infoIcon">
-          <img src="<?php echo get_template_directory_uri(); ?>//img/GitHub-Mark-64px.png" width="16" height="16" alt="" />
-        </span>
-        @ginpei
-      </a>
+      <span>
+        <a class="header-infoItem" href="/about/">
+          <span class="header-infoIcon">
+            <img src="<?= get_template_directory_uri() ?>/img/icon-32.png" width="16" height="16" alt="" />
+          </span>
+          高梨ギンペイ
+        </a>
+      </span>
+      <span>
+        <a class="header-infoItem" href="<?php bloginfo('rss2_url'); ?>">
+          <span class="header-infoIcon">📻</span>
+          RSS
+        </a>
+      </span>
+      <span>
+        <a class="header-infoItem header-infoLastItemInLine" href="https://www.google.co.jp/search?q=site%3Aginpen.com">
+          <span class="header-infoIcon">🔍</span>
+          検索
+        </a>
+      </span>
+      <span>
+        <a class="header-infoItem" href="https://twitter.com/ginpei_jp">
+          <span class="header-infoIcon">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/Twitter_Logo_Blue.svg" width="16" height="16" alt="" />
+          </span>
+          @ginpei_jp
+        </a>
+      </span>
+      <span>
+        <a class="header-infoItem" href="https://github.com/ginpei/">
+          <span class="header-infoIcon">
+            <img src="<?php echo get_template_directory_uri(); ?>//img/GitHub-Mark-64px.png" width="16" height="16" alt="" />
+          </span>
+          @ginpei
+        </a>
+      </span>
     </div>
   </div>
 </div>
