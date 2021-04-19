6ec8bb76d6a2aede6b28f01996cf2f9248a79493
ginpei <ginpei@ginpei.dev>
2021-04-18 21:12:44 -0700

    replace http: with https:

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
         <span class="header-infoIcon">🔍</span>
         検索
       </a>
-      <a class="header-infoItem" href="http://twitter.com/ginpei_jp">
+      <a class="header-infoItem" href="https://twitter.com/ginpei_jp">
         <span class="header-infoIcon">
           <img src="<?php echo get_template_directory_uri(); ?>/img/Twitter_Logo_Blue.svg" width="16" height="16" alt="" />
         </span>
