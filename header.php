6e7eac3787ed3d024d1ee4a96ee9afcc12d0719c
ginpei <ginpei@ginpei.dev>
2021-04-24 11:34:09 -0700

    update header links

--- header.php
+++ header.php
@@ -11,23 +11,11 @@
     </div>
     <div class="header-infoList">
       <span>
-        <a class="header-infoItem" href="/about/">
+        <a class="header-infoItem" href="https://ginpei.dev">
           <span class="header-infoIcon">
             <img src="<?= get_template_directory_uri() ?>/img/icon-32.png" width="16" height="16" alt="" />
           </span>
           高梨ギンペイ
-        </a>
-      </span>
-      <span>
-        <a class="header-infoItem" href="<?php bloginfo('rss2_url'); ?>">
-          <span class="header-infoIcon">📻</span>
-          RSS
-        </a>
-      </span>
-      <span>
-        <a class="header-infoItem header-infoLastItemInLine" href="https://www.google.co.jp/search?q=site%3Aginpen.com">
-          <span class="header-infoIcon">🔍</span>
-          検索
         </a>
       </span>
       <span>
@@ -44,6 +32,18 @@
             <img src="<?php echo get_template_directory_uri(); ?>//img/GitHub-Mark-64px.png" width="16" height="16" alt="" />
           </span>
           @ginpei
+        </a>
+      </span>
+      <span>
+        <a class="header-infoItem" href="<?php bloginfo('rss2_url'); ?>">
+          <span class="header-infoIcon">📻</span>
+          RSS
+        </a>
+      </span>
+      <span>
+        <a class="header-infoItem header-infoLastItemInLine" href="https://www.google.co.jp/search?q=site%3Aginpen.com">
+          <span class="header-infoIcon">🔍</span>
+          検索
         </a>
       </span>
     </div>
