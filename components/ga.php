bf28bdb4775a05435f706cb4c1e550b20c4c59da
ginpei <ginpei@gmail.com>
2022-06-02 02:42:04 +0900

    update Google Analytics tag to v4

--- components/ga.php
+++ components/ga.php
@@ -1,9 +1,10 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-21122526-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-DVMXF2H8FJ"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
+  gtag('config', 'G-DVMXF2H8FJ');
   gtag('config', 'UA-21122526-1');
 </script>
